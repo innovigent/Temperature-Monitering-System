3908cdfb71edd3354b31636d7b4bc011d8f5375e
Thushara Thiwanka <46595464+thusharathiwanka@users.noreply.github.com>
2022-01-06 14:27:29 +0530

    CREATE: total emps APIs connected

--- src/components/dashboard/TotalEmployees.js
+++ src/components/dashboard/TotalEmployees.js
@@ -9,54 +9,86 @@ import {
 import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
 import MoneyIcon from '@material-ui/icons/Money';
 import { red } from '@material-ui/core/colors';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
-const TotalEmployees = (props) => (
-  <Card sx={{ height: '100%' }} {...props}>
-    <CardContent>
-      <Grid container spacing={3} sx={{ justifyContent: 'space-between' }}>
-        <Grid item>
-          <Typography color="textSecondary" gutterBottom variant="h6">
-            TOTAL EMPLOYEES
-          </Typography>
-          <Typography color="textPrimary" variant="h3">
-            300
-          </Typography>
+const TotalEmployees = (props) => {
+  const token = localStorage.getItem('Token');
+  const headers = {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  };
+  const [count, setCount] = useState([]);
+
+  useEffect(() => {
+    const getEmps = async () => {
+      try {
+        const res = await axios.get(
+          'https://project-tnt-api.herokuapp.com/api/v1/employees/' +
+            localStorage.getItem('organization') +
+            '/getall',
+          headers
+        );
+
+        if (res.status === 200) {
+          setCount(res.data.data.employees.length);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getEmps();
+  }, []);
+
+  return (
+    <Card sx={{ height: '100%' }} {...props}>
+      <CardContent>
+        <Grid container spacing={3} sx={{ justifyContent: 'space-between' }}>
+          <Grid item>
+            <Typography color="textSecondary" gutterBottom variant="h6">
+              TOTAL EMPLOYEES
+            </Typography>
+            <Typography color="textPrimary" variant="h3">
+              {count}
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Avatar
+              sx={{
+                backgroundColor: red[400],
+                height: 56,
+                width: 56
+              }}
+            >
+              <MoneyIcon />
+            </Avatar>
+          </Grid>
         </Grid>
-        <Grid item>
-          <Avatar
-            sx={{
-              backgroundColor: red[400],
-              height: 56,
-              width: 56
-            }}
-          >
-            <MoneyIcon />
-          </Avatar>
-        </Grid>
-      </Grid>
-      <Box
-        sx={{
-          pt: 2,
-          display: 'flex',
-          alignItems: 'center'
-        }}
-      >
-        <ArrowDownwardIcon sx={{ color: red[400] }} />
-        <Typography
+        <Box
           sx={{
-            color: red[400],
-            mr: 1
+            pt: 2,
+            display: 'flex',
+            alignItems: 'center'
           }}
-          variant="body2"
         >
-          12%
-        </Typography>
-        <Typography color="textSecondary" variant="caption">
-          Since yesterday
-        </Typography>
-      </Box>
-    </CardContent>
-  </Card>
-);
+          <ArrowDownwardIcon sx={{ color: red[400] }} />
+          <Typography
+            sx={{
+              color: red[400],
+              mr: 1
+            }}
+            variant="body2"
+          >
+            12%
+          </Typography>
+          <Typography color="textSecondary" variant="caption">
+            Since yesterday
+          </Typography>
+        </Box>
+      </CardContent>
+    </Card>
+  );
+};
 
 export default TotalEmployees;
